74abaf650b30f1ab2e5f880a3f8d178007d80a1c
Mukul <mukulraj.21ug2005@iiitranchi.ac.in>
2024-09-05 19:30:37 +0530

    commit

--- server.js
+++ server.js
@@ -17,12 +17,8 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
 app.use(cors());
 
-
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error('MongoDB connection error:', err));
 
